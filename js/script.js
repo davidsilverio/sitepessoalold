9b6881ea85cafffd3f8d66d961bb9c22e722cae3
davidsilverio <43837522+davidsilverio@users.noreply.github.com>
2018-10-04 15:23:35 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* Scroll to top button */
+// Scroll to top button
 
 window.onscroll = function() {scrollFunction()};
 
@@ -14,3 +14,24 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Scrolling text
+var options = [
+    ["Passarinho que come pedra...<br>...Sabe o cu que tem"],
+    ["Apressado...<br>...come cu"],
+    ["A de amor, B de baixinho...<br>...C de coração"],
+    ["Um dia você perde...<br>...No outro você não ganha"]
+];
+
+var interval = 5000;
+
+var holder1 = $(".alternatingmotd");
+var currentIndex = 0;
+
+function doIt() {
+    holder1.html(options[currentIndex][0]);
+    currentIndex = (currentIndex + 1) % options.length;
+    setTimeout(doIt, interval);
+}
+
+doIt();
